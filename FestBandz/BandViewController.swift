627c36793faf61ae519cc08697c30b905ff1121e
Derek Jacobs <dbjacobs8@gmail.com>
2017-04-04 15:49:59 -0400

    Layout of add screen, connected w/ swift

---
+++ FestBandz/BandViewController.swift
@@ -0,0 +1,43 @@
+//
+//  BandViewController.swift
+//  FestBandz
+//
+//  Created by Derek Jacobs on 2017-04-04.
+//  Copyright © 2017 Derek Jacobs. All rights reserved.
+//
+
+import UIKit
+
+class BandViewController: UIViewController {
+
+    
+    @IBOutlet weak var bandImageView: UIImageView!
+    
+    @IBOutlet weak var festName: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func photosTapped(_ sender: Any) {
+    }
+    
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+
+}
