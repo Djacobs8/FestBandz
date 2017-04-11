611b6dc364fdcd5197a81d1bae023889fd869883
Derek Jacobs <dbjacobs8@gmail.com>
2017-04-11 00:29:27 -0400

    dunno

--- FestBandz/BandViewController.swift
+++ FestBandz/BandViewController.swift
@@ -8,27 +8,47 @@
 
 import UIKit
 
-class BandViewController: UIViewController {
+class BandViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     
     @IBOutlet weak var bandImageView: UIImageView!
     
     @IBOutlet weak var festName: UITextField!
     
+    var imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        imagePicker.delegate = self
+        //object uses a delegate to pull some info it needs
     }
 
     @IBAction func photosTapped(_ sender: Any) {
+        
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil) //places another view controller on the screen
     }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        bandImageView.image = image
+        
+        imagePicker.dismiss(animated: true, completion: nil) //dismisses the imagepicker after selection
+    }
     
     @IBAction func cameraTapped(_ sender: Any) {
+        
+      
     }
     
     @IBAction func addTapped(_ sender: Any) {
+        
+        
+        
     }
     
     
